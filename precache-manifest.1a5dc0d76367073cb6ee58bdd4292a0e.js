6add2e9f999a507f65ef2d84982b3dfa2008dc70
1446450047 <1446450047@qq.com>
2020-07-13 17:28:10 +0800

    deploy

---
+++ precache-manifest.1a5dc0d76367073cb6ee58bdd4292a0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3eae0a22052e83ab654",
+    "url": "/purse-vue-website/css/app.baf49c0e.css"
+  },
+  {
+    "revision": "a44935fda5b5bad1b3f84a3b6e555bb5",
+    "url": "/purse-vue-website/index.html"
+  },
+  {
+    "revision": "a3eae0a22052e83ab654",
+    "url": "/purse-vue-website/js/app.08a36426.js"
+  },
+  {
+    "revision": "90fe5830d2fe6bdda7fa",
+    "url": "/purse-vue-website/js/chunk-vendors.d0ad45d8.js"
+  },
+  {
+    "revision": "469a4a8ff4f6fe1756bbea1995c26407",
+    "url": "/purse-vue-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/purse-vue-website/robots.txt"
+  }
+]);
